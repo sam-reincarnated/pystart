b93514f329f5267e459e4a7bb9d6603cc2c2e8f8
Sameer <sam@master.lnet>
2023-08-25 22:14:11 +0530

    two

---
+++ level_1/script2.py
@@ -0,0 +1,10 @@
+print("Basic Math")
+# integer alph
+alpha = 100
+# integer beta
+beta = 200
+# integer theta, which is a sum of alpha and beta
+theta = alpha + beta
+# printing the sum
+print("Sum of alpha and beta is ", theta)
+
